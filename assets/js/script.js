87c540dbaaab2730403efab81ea84dc2e0979c14
g <gpxcodes@gmail.com>
2025-07-14 02:42:02 -0700

    refactor code for functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,28 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
   
     event.preventDefault();
 // retrieve task value and task type 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // package up data as object for new fuction code refactor 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskEl function 
+    createTaskEl(taskDataObj);
+    
+};
 
 
-    // create list item 
+var createTaskEl = function(taskDataObj) {
+
+     // create list item 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
@@ -19,13 +32,14 @@ var createTaskHandler = function(event) {
     // give it the class 
     taskInfoEl.className = "task-info";
     // add to HTML 
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3> <span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3> <span class='task-type'>" + taskDataObj.type + "</span>";
 
-// once we set data ^ into the div , we append it to the list here li 
+    // once we set data ^ into the div , we append it to the list here li 
     listItemEl.appendChild(taskInfoEl);
 
     // add entire list div to list (to the parent ul)
     tasksToDoEl.appendChild(listItemEl);
-};
 
-formEl.addEventListener("submit", createTaskHandler);
+}
+
+formEl.addEventListener("submit", taskFormHandler);
